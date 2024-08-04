59869fb07149069ad7c084ba5dd5578cfa4a9b2b
Rafael de Lima <rafalimabcl@gmail.com>
2024-08-03 22:06:12 -0300

    first commit

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n,m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        int[][] matriz = new int[n][m];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    System.out.println("Position " + i + "," + j + ":" );
+                    if (j > 0) {
+                        System.out.println("Left: " + matriz[i][j-1]);
+                    }
+
+                    if (i > 0) {
+                        System.out.println("Up: " + matriz[i-1][j]);
+                    }
+
+                    if (j < matriz.length - 1) {
+                        System.out.println("Right: " + matriz[i][j+1]);
+                    }
+
+                    if (i < matriz.length - 1) {
+                        System.out.println("Down: " + matriz[i+1][j]);
+                    }
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
